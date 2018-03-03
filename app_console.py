b56122eb0dd46537d509ff1d7dcb8b697da779df
chad <chattreon@gmail.com>
2018-03-03 18:18:02 +0000

    updated

--- app_console.py
+++ app_console.py
@@ -1,5 +1,17 @@
+import math
 from _01_SchoolYears import *
 from _02_Departments import *
 
+dt1 = datetime.now()
 SchoolYears.run()
+dt2 = datetime.now()
+sec = math.ceil((dt2 - dt1).total_seconds())
+print('--------------> SchoolYears: ' + str(sec) + ' sec')
+
+dt1 = datetime.now()
 Departments.run()
+dt2 = datetime.now()
+sec = math.ceil((dt2 - dt1).total_seconds())
+print('--------------> Departments: ' + str(sec) + ' sec')
+    
+    
