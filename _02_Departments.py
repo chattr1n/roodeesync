20258a44642c17ad78cc70d53cf2a73437357abf
chad <chattreon@gmail.com>
2018-03-03 19:07:44 +0000

    update departments

--- _02_Departments.py
+++ _02_Departments.py
@@ -6,27 +6,65 @@ class Departments:
     
     @staticmethod
     def get_mongo():
-        pass
+        row_list = []
+        
+        db = Driver.get_mongo()
+        results = db['app-department'].find({})
+
+        for result in results:
+            row_dict = {}
+            row_dict['ID'] = result['_id']
+            row_dict['Name'] = result['name']
+            row_dict['NameTH'] = result['th']['name']  
+            
+            row_list.append(row_dict)
+
+        return pd.DataFrame(row_list)            
         
     
     @staticmethod
     def get_mssql():
-        pass
+        
+        return Driver.get_mssql('exec spDepartmentsGet')
     
     @staticmethod
     def diff(df1, df2):
-        pass
+        key_column = 'ID'
+        
+        insert_df = df1[~df1[key_column].isin(df2[key_column])].copy()
+        update_df = df1[df1[key_column].isin(df2[key_column])].copy()        
+
+        delete_df = df2[~df2[key_column].isin(df1[key_column])].copy()
+
+        return [insert_df, update_df, delete_df]
     
     
     @staticmethod
     def upsert(upsert_df):
-        pass
+        for index, row in upsert_df.iterrows():
+            ID = row['ID']
+            Name = row['Name']
+            NameTH = row['NameTH']
+                        
+            Driver.upsert_mssql('spDepartmentsUpsert', (ID, Name, NameTH))
         
     
     @staticmethod
     def delete(delete_df):
-        pass
+        for index, row in delete_df.iterrows():
+            ID = row['ID']
+                        
+            Driver.delete_mssql('spDepartmentsDelete', (ID,))
     
     @staticmethod
     def run():
-        pass
+        pd.set_option('display.width', 1000)
+        
+        df1 = Departments.get_mongo()
+        df2 = Departments.get_mssql()
+        
+        [insert_df, update_df, delete_df] = Departments.diff(df1, df2)
+                    
+        Departments.upsert(insert_df)
+        Departments.upsert(update_df)
+        Departments.delete(delete_df)
